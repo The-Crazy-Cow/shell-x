5301bbbf894f030fcec3323d527a1fdb62fbbdf2
jd <jd@localhost.localdomain>
2026-08-24 00:04:30 +0200

    shell-x/include - add errors handling macros and useful error's handler

---
+++ include/errors.h
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#ifndef INCLUDE_ERRORS_H
+#define INCLUDE_ERRORS_H
+
+#if defined(DEBUG)
+#include "debug.h"
+
+//errors
+#define ER_NULL_PTR pr_error("Null pointer")
+
+//warnings
+
+//avoiding buffer overflow by truncating the buffer to the maximum length defined in prompt.h
+#define WR_TRUNCATE_BUFFER pr_warn("Truncate buffer")
+
+#endif /*DEBUG*/
+
+//IS_NULL_PTR MACRO  contains a debug option so prefer the compare to null in often cas
+#define IS_NULL_PTR(ptr)                                                      \
+	({                                                                        \
+		__typeof__(ptr) _ptr = (ptr);                                         \
+		int _is_null = (_ptr == NULL);                                        \
+		if (_is_null)                                                         \
+			pr_error("Null pointer: %s", #ptr);                              \
+		_is_null;                                                             \
+	})
+
+#endif /*INCLUDE_ERRORS_H*/
